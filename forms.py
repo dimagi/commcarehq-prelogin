1178087e3978afdeb523db3f1f3ef1d93e67b0c3
Biyeun Buczyk <biyeun@dimagi.com>
2015-07-29 16:18:15 -0400

    async sending of email, and add option to send to supply email

--- forms.py
+++ forms.py
@@ -3,16 +3,17 @@ from django import forms
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy, ugettext as _
 from django.conf import settings
-from dimagi.utils.django.email import send_HTML_email
 
 # Bootstrap 3 Crispy Forms
 from crispy_forms import layout as cb3_layout
 from crispy_forms import helper as cb3_helper
 from crispy_forms import bootstrap as twbscrispy
 from corehq.apps.style import crispy as hqcrispy
-
+from corehq.apps.hqwebapp.tasks import send_html_email_async
 
 logger = logging.getLogger(__name__)
+
+SOLUTIONS_CONTACT_EMAIL = "commcare.supply@dimagi.com"
 
 
 class ContactDimagiForm(forms.Form):
@@ -101,17 +102,18 @@ class ContactDimagiForm(forms.Form):
             )
         )
 
-    def send_email(self):
+    def send_email(self, is_solutions_contact=False):
         try:
             params = {
                 'contact_form': self,
             }
             html_content = render_to_string("prelogin/_email/contact_form_email.html", params)
             text_content = render_to_string("prelogin/_email/contact_form_email.txt", params)
-            recipient = settings.CONTACT_EMAIL
+            recipient = SOLUTIONS_CONTACT_EMAIL if is_solutions_contact else settings.CONTACT_EMAIL
             subject = "Contact Dimagi Request from prelogin"
-            send_HTML_email(subject, recipient, html_content, text_content=text_content,
-                            email_from=settings.DEFAULT_FROM_EMAIL)
+            send_html_email_async.delay(
+                subject, recipient, html_content, text_content=text_content,
+                email_from=settings.DEFAULT_FROM_EMAIL)
         except Exception:
             logging.error("Couldn't send pre-login contact email. "
                           "Contact: %s" % self.cleaned_data['contact_email']
