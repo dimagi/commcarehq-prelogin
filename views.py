5df94f66ea3c63c951e790d1aed06e7653b7674a
Biyeun Buczyk <biyeun@dimagi.com>
2015-07-31 17:16:57 -0400

    remove old contact us form and add form submitted via hubspot

--- views.py
+++ views.py
@@ -6,8 +6,6 @@ from django.utils.safestring import mark_safe
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
-from djangular.views.mixins import allow_remote_invocation, JSONResponseMixin
-from corehq.apps.prelogin.forms import ContactDimagiForm
 from corehq.apps.style.decorators import use_bootstrap3
 
 
@@ -15,28 +13,11 @@ def public_default(request):
     return HttpResponseRedirect(reverse(HomePublicView.urlname))
 
 
-class BasePreloginView(JSONResponseMixin, TemplateView):
-    """Have the contact dimagi form logic present always, as pretty much
-    every page needs access to it.
-    """
-    is_solutions_contact = False
+class BasePreloginView(TemplateView):
 
     @method_decorator(use_bootstrap3())
     def dispatch(self, request, *args, **kwargs):
         return super(BasePreloginView, self).dispatch(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        kwargs['contact_form'] = ContactDimagiForm()
-        return super(BasePreloginView, self).get_context_data(**kwargs)
-
-    @allow_remote_invocation
-    def send_email(self, in_data):
-        contact_form = ContactDimagiForm(in_data)
-        if contact_form.is_valid():
-            contact_form.send_email(is_solutions_contact=self.is_solutions_contact)
-            return {'success': True}
-        else:
-            return {'success': False, 'errors': contact_form.errors}
 
 
 class HomePublicView(BasePreloginView):
@@ -100,7 +81,6 @@ class ServicesDetailsPublicView(BasePreloginView):
 class SolutionsPublicView(BasePreloginView):
     urlname = 'public_solutions'
     template_name = 'prelogin/solutions.html'
-    is_solutions_contact = True
 
     def get_context_data(self, **kwargs):
         kwargs['is_solutions'] = True
