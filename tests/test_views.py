88579154b3cf4375329a632e110110bba6f937a1
Sravan Reddy <sravfeyn@gmail.com>
2015-12-25 18:15:28 +0530

    tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,7 +1,8 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 from corehq.apps.prelogin.views import *
 
-class TestViews(TestCase):
+
+class TestViews(SimpleTestCase):
     def test_status_codes(self):
         for view in [
             HomePublicView,
@@ -13,3 +14,24 @@ class TestViews(TestCase):
         ]:
             response = self.client.get(reverse(view.urlname), follow=False)
             self.assertEqual(response.status_code, 200)
+
+
+class TestLanguagePrefixes(SimpleTestCase):
+    def test_prefixed_pages_get_translated(self):
+        for lang in ['en', 'fra']:
+            prefixed_url = reverse(HomePublicView.urlname, args=[lang])
+            response = self.client.get(prefixed_url, follow=False)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response._headers['content-language'][1], lang)
+
+    def test_unknown_lang_redirects(self):
+        unknown_lang_url = reverse(HomePublicView.urlname, args=['random_lang'])
+        response = self.client.get(unknown_lang_url, follow=False)
+        # should redirect to same URL without prefix
+        self.assertRedirects(
+            response,
+            reverse(HomePublicView.urlname),
+            status_code=302,
+            fetch_redirect_response=True,
+            target_status_code=200
+        )
