c235f1eec1d5eaa60c358335c501b56fe6fb0ddb
Sravan Reddy <sravfeyn@gmail.com>
2016-01-04 16:45:40 +0530

    use includes

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from django.views.generic import RedirectView
 from corehq.apps.prelogin.views import *
 
@@ -17,17 +17,8 @@ root_patterns = patterns(
     url(r'^supply/$', RedirectView.as_view(url='/solutions/#supply', permanent=True)),
 )
 
+
 urlpatterns = root_patterns + patterns(
     'corehq.apps.prelogin.views',
-    url(r'^lang/(?P<lang_code>[\w-]+)/home/$', HomePublicView.as_view(), name=HomePublicView.urlname),
-    url(r'^lang/(?P<lang_code>[\w-]+)/impact/$', ImpactPublicView.as_view(), name=ImpactPublicView.urlname),
-    url(r'^lang/(?P<lang_code>[\w-]+)/software_services/$', SoftwareServicesPublicView.as_view(),
-        name=SoftwareServicesPublicView.urlname),
-    url(r'^lang/(?P<lang_code>[\w-]+)/services/$', ServicesPublicView.as_view(),
-        name=ServicesPublicView.urlname),
-    url(r'^lang/(?P<lang_code>[\w-]+)/pricing/$', PricingPublicView.as_view(),
-        name=PricingPublicView.urlname),
-    url(r'^lang/(?P<lang_code>[\w-]+)/solutions/$', SolutionsPublicView.as_view(),
-        name=SolutionsPublicView.urlname),
-    url(r'^lang/(?P<lang_code>[\w-]+)/supply/$', RedirectView.as_view(url='/solutions/#supply', permanent=True)),
+    url(r'^lang/(?P<lang_code>[\w-]+)/', include(root_patterns)),
 )
