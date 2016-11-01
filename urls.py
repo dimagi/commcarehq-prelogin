fd43fce6751a18a14ebc0127e9c52a94baf3ba8d
Nick Pellegrino <npellegrino@dimagi.com>
2016-11-01 10:52:04 -0400

    make urlpatterns into a list

--- urls.py
+++ urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from django.views.generic import RedirectView
 from corehq.apps.prelogin.views import *
 
-root_patterns = patterns(
-    'corehq.apps.prelogin.views',
+root_patterns = [
     url(r'^home/$', HomePublicView.as_view(), name=HomePublicView.urlname),
     url(r'^demo/$', DemoPublicView.as_view(), name=DemoPublicView.urlname),
     url(r'^impact/$', ImpactPublicView.as_view(), name=ImpactPublicView.urlname),
@@ -18,10 +17,9 @@ root_patterns = patterns(
     url(r'^solutions/$', SolutionsPublicView.as_view(),
         name=SolutionsPublicView.urlname),
     url(r'^supply/$', RedirectView.as_view(url='/solutions/#supply', permanent=True)),
-)
+]
 
 
-urlpatterns = root_patterns + patterns(
-    'corehq.apps.prelogin.views',
+urlpatterns = root_patterns + [
     url(r'^lang/(?P<lang_code>[\w-]+)/', include(root_patterns)),
-)
+]
